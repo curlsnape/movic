1fcdce94b8d91b560335f223ea73a6739035a4a9
Akshay Sawant <1akshaysawant@gmail.com>
2025-02-07 19:54:28 +0530

    commit

--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -4,7 +4,6 @@ import Topnav from "../Partials/Topnav";
 import Dropdown from "../Partials/Dropdown";
 import Cards from "../Partials/Cards";
 import axios from "../utils/Axios";
-import Loader from "../Partials/Loader";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const Trending = () => {
@@ -20,27 +19,34 @@ const Trending = () => {
       const { data } = await axios.get(
         `/trending/${category}/${duration}?page=${page}`
       );
-      if (data.results.length > 0) {
-        setTrending((prev) => [...prev, ...data.results]);
-        setPage((prevPage) => prevPage + 1);
-      } else {
-        setHasMore(false);
-      }
+     if(data.results.length>0){
+       setTrending((prev) => [...prev, ...data.results]);
+       setPage((prevPage) => prevPage + 1);
+     }
+     else{
+      hasMore(false)
+     }
+
     } catch (error) {
       console.log(error);
     }
   };
-
+  const refereshHandler = () => {
+    if (trending.length === 0) {
+      getTrending()
+    }
+    else {
+      setPage(1)
+      setTrending([])
+      getTrending()
+    }
+  }
   useEffect(() => {
-    // Reset state and fetch new data when category or duration changes
-    setPage(1);
-    setTrending([]);
-    setHasMore(true);
-    getTrending();
+    refereshHandler()
   }, [category, duration]);
 
   return (
-    <div className="w-full h-full overflow-y-auto scroll-smooth px-20">
+    <div className="w-full min-h-screen bg-zinc-900  px-20">
       <div className="nav w-full h-[10vh] flex items-center justify-between">
         <div className="logo flex text-xl items-center">
           <i
@@ -53,18 +59,20 @@ const Trending = () => {
           <Topnav />
           <Dropdown
             title="Filter"
-            func={(e)=>setCategory(e.target.value)}
+            func={(e) => setCategory(e.target.value)}
             options={["all", "tv", "movie"]}
           />
           <div className="w-[10px]"></div>
           <Dropdown
-            title="Filter"
-            func={(e)=>setDuration(e.target.value)}
+            title="Duration"
+            func={(e) => setDuration(e.target.value)}
             options={["day", "week"]}
           />
         </div>
       </div>
+      <InfiniteScroll dataLength={trending.length} hasMore={hasMore} next={getTrending} loader={<h1>Loading...</h1>}>
         <Cards data={trending} />
+      </InfiniteScroll>
     </div>
   );
 };
